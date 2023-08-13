bf7bd4aa0b4b43d7d8700a237b1f9893011ecf8e
vegone-xr <vegone@protonmail.com>
2023-08-13 17:17:18 +0200

    0.1.77

---
+++ src/html/index.ts
@@ -0,0 +1,12 @@
+///// GENERATED FILE /////
+
+export * from './addCssFile';
+export * from './addJsFile';
+export * from './getAttrs';
+export * from './getCls';
+export * from './htmlToEl';
+export * from './interfaces';
+export * from './setAttrs';
+export * from './setCls';
+export * from './setCss';
+export * from './setSiteTitle';
--- src/record/index.ts
+++ src/record/index.ts
@@ -1,5 +1,8 @@
 ///// GENERATED FILE /////
 
 export * from './deleteKey';
+export * from './entries';
+export * from './fromEntries';
+export * from './fromKeys';
 export * from './groupBy';
 export * from './sortKey';
